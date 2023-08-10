4c1b18660db42cc6620b6a6b9e7dd27dfeee1966
AnnaFridman2512 <AnnaFridman81@gmail.com>
2023-08-10 13:49:27 +0300

    Task2 finished

--- 2/solution.py
+++ 2/solution.py
@@ -1,2 +1,12 @@
 def main(str):
-  return "not implemented yet"
+
+    reversed_words = []
+    words = str.split()
+
+    for word in words:
+        reversed_word = ''.join(reversed(word))
+        reversed_words.append(reversed_word)
+
+    reversed_words.reverse()  # Reverse the list of reversed words
+
+    return ' '.join(reversed_words)
