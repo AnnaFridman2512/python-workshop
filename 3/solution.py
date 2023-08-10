487e9d31477c04b7c3fa1826d47872a1f28a6c07
AnnaFridman2512 <AnnaFridman81@gmail.com>
2023-08-10 14:08:46 +0300

    task3 finished

--- 3/solution.py
+++ 3/solution.py
@@ -1,2 +1,6 @@
+import re
+
 def main(sentence):
-  return "not implemented yet"
+    words = re.split(r'[ ,.;:\'"(){}]+', sentence)  # Split the sentence using regular expression
+    longest_word = max(words, key=len)  # Find the longest word based on length
+    return longest_word
