563c0bd5cebe691a1489441fb0b53213825276f5
AnnaFridman2512 <AnnaFridman81@gmail.com>
2023-08-10 17:50:10 +0300

    Task 5 finished

--- 5/solution.py
+++ 5/solution.py
@@ -1,2 +1,18 @@
 def main(seconds):
-  return "not implemented yet"
+    # Determine if the number is negative
+    is_negative = False
+    if seconds < 0:
+        is_negative = True
+        seconds = -seconds
+
+    # Calculate hours, minutes, and seconds
+    hours, remainder = divmod(seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    # Format the result string
+    result = "{:0>1}:{:0>2}:{:0>2}".format(hours, minutes, seconds)
+
+    # Add negative sign if the number was negative
+    if is_negative:
+        return "-" + result
+    return result
