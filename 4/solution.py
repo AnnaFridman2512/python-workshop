5cdee96e818e25b3c0205e576dc848b76440fa68
AnnaFridman2512 <AnnaFridman81@gmail.com>
2023-08-10 16:38:56 +0300

    Task4 finished

--- 4/solution.py
+++ 4/solution.py
@@ -1,2 +1,26 @@
-def main(str):
-  return "not implemented yet"
+import re
+
+def main(s):
+    # Find sequences of spaces or punctuations
+    delimiters = re.findall(r'\s+|[ ,;.]+', s)
+
+    # Split the string based on the found delimiters
+    words = re.split(r'\s+|[ ,;.]+', s)
+
+    # Process each word
+    processed_words = []
+    for word in words:
+        if word and word[0].isalpha() and word[0] in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ':
+            processed_words.append(word.capitalize())
+        else:
+            processed_words.append(word.lower())
+
+    # Interleave the processed words with the delimiters
+    result = []
+    for i in range(len(processed_words)):
+        result.append(processed_words[i])
+        if i < len(delimiters):  # Ensure we don't run out of delimiters
+            result.append(delimiters[i])
+
+    # Join everything together
+    return ''.join(result)
