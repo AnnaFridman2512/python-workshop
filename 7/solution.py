a970ae75513302da67af629898613ffd4ab463b7
AnnaFridman2512 <AnnaFridman81@gmail.com>
2023-08-10 18:34:12 +0300

    Task7 finished

--- 7/solution.py
+++ 7/solution.py
@@ -1,2 +1,17 @@
-def main(word,parts):
-  return "not implemented yet"
+def main(word, parts):
+    # Convert the comma-separated string into a list
+    parts_list = parts.split(',')
+
+    # Iterate through the word to find a split where both parts exist in parts_list
+    for i in range(1, len(word)):
+        first_part = word[:i]
+        second_part = word[i:]
+
+        # Handle case where both parts are the same
+        if first_part == second_part:
+            if parts_list.count(first_part) >= 2:
+                return [first_part, second_part]
+        elif first_part in parts_list and second_part in parts_list:
+            return [first_part, second_part]
+
+    return []
