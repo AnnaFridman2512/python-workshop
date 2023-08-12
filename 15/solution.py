16f8a14260c54e89c429a0f63786dbfc87636e6c
AnnaFridman2512 <AnnaFridman81@gmail.com>
2023-08-12 16:46:06 +0300

    Task 15 finished

--- 15/solution.py
+++ 15/solution.py
@@ -1,2 +1,9 @@
 def main(str):
-    return ""
+    result = []
+    # Loop over each character with its position (1-indexed)
+    for index, char in enumerate(str, start=1):
+        # Repeat the character, capitalize the first occurrence, and append to the result
+        transformed_char = (char * index).capitalize()
+        result.append(transformed_char)
+
+    return '-'.join(result)
