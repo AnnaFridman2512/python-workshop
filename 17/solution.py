69d05fe95f61a70b753f230da6724d3b43ce93b0
AnnaFridman2512 <AnnaFridman81@gmail.com>
2023-08-12 18:46:07 +0300

    task 17 finished

--- 17/solution.py
+++ 17/solution.py
@@ -1,2 +1,18 @@
+import re
+
 def main(txt):
-  return "not implemented yet"
+    #Extract emails from text
+    email_pattern = re.compile(r'[a-zA-Z0-9\.]+@[a-zA-Z0-9\.]+')
+    emails = re.findall(email_pattern, txt)
+
+    #Remove duplicates (using dictionary to preserve order)
+    email_dict = {}
+    for email in emails:
+        lower_email = email.lower()
+        if lower_email not in email_dict:
+            email_dict[lower_email] = email
+
+    # Step 3: Sort emails by domain and then by name
+    sorted_emails = sorted(email_dict.values(), key=lambda x: (x.split('@')[1], x.split('@')[0]))
+
+    return sorted_emails
