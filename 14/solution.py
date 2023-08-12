fe6a78ae315a6e38d662b43f1170c88f7316a9e7
AnnaFridman2512 <AnnaFridman81@gmail.com>
2023-08-12 16:35:55 +0300

    task 14 finished

--- 14/solution.py
+++ 14/solution.py
@@ -1,2 +1,10 @@
-def main(recipe, available):
-    return -1
+def main(recipe, available_ingredients):
+    # Calculate the ratio for each ingredient in the recipe
+    ratios = []
+    for ingredient, amount in recipe.items():
+        if ingredient in available_ingredients:
+            ratios.append(available_ingredients[ingredient] // amount)
+        else:
+            return 0  # Ingredient not present, so can't bake even one cake.
+    return min(ratios)  # Return the smallest ratio, which indicates max number of cakes.
+
